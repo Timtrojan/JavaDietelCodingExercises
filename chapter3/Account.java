8aca07f0193bf190732570ae812f8452d16e4108
Adegbasa Timothy <timothyadegbasa@gmail.com>
2022-11-10 17:12:21 +0100

    <this is an account class initial commit>

---
+++ chapter3/Account.java
@@ -0,0 +1,36 @@
+public class Account{
+
+private String name;
+private int balance;
+
+
+public Account(){
+
+}
+public Account(String name, int balance){
+this.name=name;
+this.balance=balance;
+
+
+}
+
+Public int checkBalance(){
+return balance;
+
+}
+
+public void checkName(){
+return name;
+
+}
+
+public void setName(){
+
+
+}
+
+
+
+
+
+}
